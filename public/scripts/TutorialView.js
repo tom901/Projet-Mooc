251510f7690f9b583425df3b5e95e8817021586f
tom901 <thomas.paraiso@me.com>
2015-10-14 11:19:32 +0200

    Separation du header avec le fichier principale

--- public/scripts/TutorialView.js
+++ public/scripts/TutorialView.js
@@ -1,25 +1,3 @@
- var Header = React.createClass({
-  render : function(){
-    return(
-        <div className="headerComponent">
-            <div className="blog-masthead">
-              <div className="container">
-                <nav className="blog-nav">
-                  <a className="blog-nav-item active" target="_blank" href="https://twitter.com/ThomAiso">PROJET MOOC</a>
-                </nav>
-              </div>
-            </div>
-            <div className="container">
-                <div className="blog-header">
-                    <h1 className="blog-title">Liste de tutoriels</h1>
-                    <p className="lead blog-description">Tutoriels sur différents sujets concernant Salesforce, Javascript et les nouveaux utilitaires existants.</p>
-                </div>
-            </div>
-        </div>  
-        )
-    }
-});
-
  var TutorialAside = React.createClass({
     render : function(){   
         return(
@@ -99,7 +77,6 @@
     render: function() {
         return (
             <div className="tutorialBox">
-                <Header />
                 <div className="tutorial">
                     <div className="container">
                         <div className="row">
