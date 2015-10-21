56d181c224bb0b14506ecf455ef00f7332d9a623
LESLIE <leslie.cha@hotmail.fr>
2015-10-21 15:57:13 +0200

    Page consulter exercice: ajout de la liste des exo et du logo JS

--- react_components/consultexercicespage.js
+++ react_components/consultexercicespage.js
@@ -1,9 +1,77 @@
  module.exports = React.createClass({
+    
 	render : function(){ 
 		return (
-			<div style={{margin:'auto',width:'400px'}} >
-			<h2> La page consultation des exercices</h2>
-			</div>
+
+            <div className="container">
+
+                <div className="row">
+                    <div className="col-lg-12">
+                        <h3>Liste des exercices</h3>
+
+                    </div>
+                </div>
+
+                <div className="row text-center">
+
+                    <div className="col-md-3 col-sm-6 hero-feature">
+                        <div className="thumbnail">
+                            <img style={{width:'50px'}} src="./images/Logo-JS.png" alt="" />
+                            <div className="caption">
+                                <h3>Exercice 1</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                                <p>
+                                    <a href="#" className="btn btn-primary">{"Commencer l'exercice"}</a> <a href="#" className="btn btn-default">Voir la correction</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-md-3 col-sm-6 hero-feature">
+                        <div className="thumbnail">
+                            <img style={{width:'50px'}} src="./images/Logo-JS.png" alt="" />
+                            <div className="caption">
+                                <h3>Exercice 2</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                                <p>
+                                    <a href="#" className="btn btn-primary">{"Commencer l'exercice"}</a> <a href="#" className="btn btn-default">Voir la correction</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-md-3 col-sm-6 hero-feature">
+                        <div className="thumbnail">
+                            <img style={{width:'50px'}} src="./images/Logo-JS.png" alt="" />
+                            <div className="caption">
+                                <h3>Exercice 3</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                                <p>
+                                    <a href="#" className="btn btn-primary">{"Commencer l'exercice"}</a> <a href="#" className="btn btn-default">Voir la correction</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-md-3 col-sm-6 hero-feature">
+                        <div className="thumbnail">
+                            <img style={{width:'50px'}} src="./images/Logo-JS.png" alt="" />
+                            <div className="caption">
+                                <h3>Exercice 4</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                                <p>
+                                    <a href="#" className="btn btn-primary">{"Commencer l'exercice"}</a> <a href="#" className="btn btn-default">Voir la correction</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <hr />
+
+            </div>
+
 		)
 	}
 });
