f1e0debefb38f88dbdf74b045ab000104bd8bf6d
Alex <cad.maxime@gmail.com>
2015-10-26 16:36:32 +0100

    Essaies graphiques 2

--- react_components/connected.js
+++ react_components/connected.js
@@ -15,40 +15,38 @@ module.exports = React.createClass({
        return (
            <div>
                <aside>
-                  <div id="sidebar" className="nav-collapse " style={sidebarStyle}>
+                  <div id="sidebar" className="nav-collapse sidebar-hack" style={sidebarStyle}>
                       <ul className="sidebar-menu" id="nav-accordion" style={displayBlockStyle}>
-
                       	  <p className="centered">
                               <a href="profile.html">
                                   <img src={"https://s.gravatar.com/avatar/" + md5('adresse@email.fr') + "?s=100" } className="img-circle" width="100" /></a></p>
                       	  <h5 className="centered">Bonjour, John Smith</h5>
-
                           <li className="mt">
-                              <a className="active" href="javascript:;">
+                              <a href="#home" aria-controls="home" role="tab" data-toggle="tab">
                                   <i className="fa fa-dashboard"></i>
-                                  <span>Mon profil</span>
+                                  <span>Dashboard</span>
                               </a>
                           </li>
                           <li className="sub-menu dcjq-parent-li">
-                              <a href="javascript:;" className="dcjq-parent">
+                              <a href="#documentation" aria-controls="documentation" role="tab" data-toggle="tab" className="dcjq-parent">
                                   <i className="fa fa-book"></i>
                                   <span>Documentation</span>
                               </a>
                           </li>
                           <li className="sub-menu dcjq-parent-li">
-                              <a href="javascript:;" className="dcjq-parent">
+                              <a href="#cours" aria-controls="cours" role="tab" data-toggle="tab" className="dcjq-parent">
                                   <i className="fa fa-cogs"></i>
                                   <span>Cours</span>
                               </a>
                           </li>
                           <li className="sub-menu dcjq-parent-li">
-                              <a href="javascript:;" className="dcjq-parent">
+                              <a href="#exercices" aria-controls="exercices" role="tab" data-toggle="tab" className="dcjq-parent">
                                   <i className="fa fa-tasks"></i>
                                   <span>Exercices</span>
                               </a>
                           </li>
                           <li className="sub-menu dcjq-parent-li">
-                              <a href="javascript:;" className="dcjq-parent">
+                              <a href="#performances" aria-controls="performances" role="tab" data-toggle="tab" className="dcjq-parent">
                                   <i className=" fa fa-bar-chart-o"></i>
                                   <span>Performances</span>
                               </a>
@@ -57,6 +55,42 @@ module.exports = React.createClass({
                       </ul>
                   </div>
               </aside>
+              <div className="container-fluid container-fluid-with-aside">
+                  <div className="tab-content">
+                      <div role="tabpanel" className="tab-pane fade in active" id="home">
+                          <div className="row text-center">
+                              <div className="col-md-3 col-sm-6">
+                                  avancement
+                              </div>
+
+                              <div className="col-md-3 col-sm-6 hero-feature">
+                                  moyenne
+                              </div>
+
+                              <div className="col-md-3 col-sm-6 hero-feature">
+                                  ...
+                              </div>
+
+                              <div className="col-md-3 col-sm-6 hero-feature">
+                                  ...
+                              </div>
+                          </div>
+                          <h2>Bienvenue sur le site des exercices à correction automatique.</h2>
+                          <p>
+                              Si vous venez pour la première fois,
+                              sachez que vous pouvez changer votre pseudo,
+                              votre courriel et votre mot de passeen clicquant sur le menu « Profil »
+                               puis sur « Modifier mon profil ».
+                               <br />
+                               De nouveaux exercices vous seront proposés au cours de l'année. Bon travail!
+                          </p>
+                      </div>
+                      <div role="tabpanel" className="tab-pane fade in " id="documentation">documentation</div>
+                      <div role="tabpanel" className="tab-pane fade in " id="cours">cours</div>
+                      <div role="tabpanel" className="tab-pane fade in " id="exercices">exercices</div>
+                      <div role="tabpanel" className="tab-pane fade in " id="performances">performances</div>
+                  </div>
+              </div>
            </div>
        )
    }
